86f167cad6ab0b64ef2db97b29fad7735f906b52
Stas Okhrym <105078166+StasOkhrym@users.noreply.github.com>
2024-06-24 17:11:37 +0300

    Initial Commit

---
+++ Clip/ClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipApp.swift
+//  Clip
+//
+//  Created by Станіслав Охрим on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clip
+//
+//  Created by Станіслав Охрим on 24.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
